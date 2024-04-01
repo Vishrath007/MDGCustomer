7f4a2df5f43ac0e81c164dd8796cff3b890a425b
Vishrath007 <165700021+Vishrath007@users.noreply.github.com>
2024-04-02 00:23:28 +0530

    Add files via upload

---
+++ MDGCustomer/src/main/java/com/mdgcustomer/hpcl/MdgCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.mdgcustomer.hpcl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdgCustomerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdgCustomerApplication.class, args);
+	}
+
+}
---
+++ MDGCustomer/src/test/java/com/mdgcustomer/hpcl/MdgCustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdgcustomer.hpcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdgCustomerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
